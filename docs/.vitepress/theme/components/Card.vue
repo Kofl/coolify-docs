ba528a068bcb6715c5bf27a5bc1bc3ebd507c6ee
ShadowArcanist <162910371+ShadowArcanist@users.noreply.github.com>
2026-01-21 05:32:48 +0100

    docs(home): updated new home page content

--- docs/.vitepress/theme/components/Card.vue
+++ docs/.vitepress/theme/components/Card.vue
@@ -3,37 +3,25 @@ import { computed } from 'vue'
 const props = defineProps<{
     title?: string
     image?: string
-    imageAlt?: string
-    imageSize?: 'small' | 'medium' | 'large'
     link?: string
     icon?: string
     iconSize?: 'small' | 'medium' | 'large'
     iconColor?: string
     theme?: 'outlined' | 'solid' | 'icon'
+    cols?: number | 'auto'
 }>()
-
-const imageSizeClass = computed(() => ({
-    small: 'w-[50px] h-[50px]',
-    medium: 'w-[100px] h-[100px]',
-    large: 'w-[150px] h-[150px]'
-}[props.imageSize ?? 'medium']))
 </script>
 
 <template>
-  <div class="group p-1 bg-gray-50 rounded-lg dark:bg-gray-800 border transition-all duration-200 border-gray-50 dark:border-gray-800 hover:border-purple-500 dark:hover:border-purple-700">
-    <div class="flex flex-col h-[225px] dark:bg-gray-800 rounded-lg cursor-pointer transition-transform duration-200">
-      <div class="flex-[3] bg-linear-to-r from-[#00D1EE] via-[#08ABDD] via-[#15ACF1] via-[#1575C1] to-[#14336d] rounded-lg flex items-center justify-center">
-        <img 
-          :src="image" 
-          :alt="imageAlt"
-          :class="[
-            imageSizeClass,
-            'p-1 rounded-lg text-white',
-            'bg-[#18222EDF]/70 shadow-xl'
-          ]"
+  <a :href="link" class="group p-1 bg-gray-100 border border-gray-200 rounded-lg dark:bg-zinc-900 dark:border-zinc-800 hover:shadow-md hover:bg-gray-100 dark:hover:bg-zinc-800 transition-all duration-200 no-underline" style="border-bottom: none;">
+    <div class="flex flex-col rounded-lg cursor-pointer transition-transform duration-200">
+      <div :class="cols && typeof cols === 'number' && cols > 2 ? 'aspect-[3/2] rounded-lg overflow-hidden' : 'aspect-video rounded-lg overflow-hidden'">
+        <img
+          :src="image"
+          class="w-full h-full object-cover"
         />
       </div>
-      <div class="flex items-center justify-between px-4">
+      <div class="flex items-center justify-between px-4 pt-1 pb-0">
         <div class="flex items-center gap-2">
           <p class="text-base font-medium m-0 text-gray-800 dark:text-gray-100">{{ title }}</p>
           <div class="group-hover:translate-x-1 transition-all duration-200 w-4 h-4 text-gray-500 dark:text-gray-400">
@@ -44,7 +32,7 @@ const imageSizeClass = computed(() => ({
         </div>
       </div>
     </div>
-  </div>
+  </a>
 </template>
 
 
